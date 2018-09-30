03fc5dcf7015bb91abd39d866949256d6313a442
Robert Schnitman <robertschnitman@gmail.com>
2018-09-29 20:51:48 -0500

    Add files via upload

---
+++ R/dedup.R
@@ -0,0 +1,20 @@
+# Removing and finding duplicates.
+# duplicated() is slow. How to make it fast like dplyr::distinct?
+
+dedup <- function(x, ...) {
+  
+  # x[!duplicated(x, ...), ] # 0.02 second slower than x[!duplicated(x), ] manually.
+  
+  unique(x) # 0.01 second faster than the same.
+  
+}
+
+dup_rows <- function(x) x[duplicated(x), ]
+
+dup_nrow <- function(x) NROW(dup_rows(x))
+
+dup_indc <- function(x) {
+  
+  ifelse(duplicated(x) == TRUE, 1, 0)
+  
+}
