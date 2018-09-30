d7817bc3ee5a789500baefed54a855e4aad2f893
Robert Schnitman <robertschnitman@gmail.com>
2018-09-29 22:15:29 -0500

    Generalize
    NA is tricky: just separate it into its on control flow branch.

--- R/recode.R
+++ R/recode.R
@@ -1,18 +1,32 @@
 # recode a la` SPSS.
 # dplyr::recode is a vectorized switch().
 # Make recode() more like replace(),
-#   where the "list" input is a set of raw values to find instead of indices.
+#   where the "list" input is a set of raw values to initial instead of indices.
 
-recode <- function(x, find, new) { # assumes find/new are in the appropriate order.
+recode <- function(x, initial, new) { # assumes initial/new are in the appropriate order.
   
-  stopifnot(length(find) == length(new))
+  stopifnot(length(initial) == length(new))
   
-  for (i in 1:length(find)) {
+  for (i in 1:length(initial)) {
     
-    x[x == find[i]] <- new[i]
+    if (is.na(initial[i])) {
+      
+      x[is.na(x)] <- new[i]
+      
+    } else {
+      
+      x[x == initial[i]] <- new[i]
+      
+    }
     
   }
   
   x
   
 }
+
+
+recodeNA  <- function(x, v) recode(x, NA, v)
+recodeNA0 <- function(x) recodeNA(x, 0)
+
+nvl <- recodeNA # similar to SQL, but for NAs. Shorthand.
