b93bbce12ca72a94cf3bb738e4a1a0027092edf3
Robert Schnitman <robertschnitman@gmail.com>
2018-09-29 22:04:55 -0500

    Generalize
    If the user wants the *total* number of missing values, leave the margin input null; otherwise, compute number of missing values by row or column.

---
+++ R/numNA.R
@@ -0,0 +1,21 @@
+# a la` Stata's egen rowmiss().
+
+numNA <- function(x, m = NULL) {
+  
+  lenna <- function(y) length(is.na(y)[is.na(y)])
+  
+  if (is.null(m)) {
+    
+    output <- lenna(x)
+    
+  } else {
+    
+    output <- apply(x, m, lenna)
+  }
+  
+  output
+  
+}
+
+rowNA <- function(x) numNA(x, 1)
+colNA <- function(x) numNA(x, 2)
