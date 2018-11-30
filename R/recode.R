998aab551a4cffa222d488b8110c6a5f2988076d
Robert Schnitman <robertschnitman@gmail.com>
2018-11-30 16:06:41 -0600

    Be concise; avoid confusion with SQL's nvl()
    1. The for loop in recode() was obtuse: replaced with...replace() to be parsimonious.
    
    2. SQL's nvl() are for NULL values: avoid confusing readers by renaming the NA-related functions appropriately.

--- R/recode.R
+++ R/recode.R
@@ -3,37 +3,24 @@
 # Make recode() more like replace(),
 #   where the "list" input is a set of raw initial values to find instead of indices.
 
+
 recode <- function(x, initial, new) { # assumes initial/new are in the appropriate order.
   
   stopifnot(length(initial) == length(new))
-
-  for (i in 1:length(initial)) {
-    
-    if (is.na(initial[i])) {
-      
-      x[is.na(x)] <- new[i]
-      
-    } else {
-      
-      x[x == initial[i]] <- new[i]
-      
-    }
-    
-  }
   
-  x
+  replace(x, x %in% initial, new)
   
 }
 
 
-nvl <- function(x, v) { # similar to SQL, but for NAs. Shorthand.
+NAvl <- function(x, v) { # similar to SQL's nvl(), but for NAs. Shorthand.
 
   ifelse(is.na(x), v, x)
   
 }
 
-nvl0 <- function(x) { # Switch all NAs to 0.
+NAvl0 <- function(x) { # Switch all NAs to 0.
   
-  ifelse(is.na(x), 0, x)
+  NAvl(x, 0)
   
 }
