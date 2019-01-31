139cf64cf0648e1cbc2371fa3412b8e3ea0decf4
Robert Schnitman <robertschnitman@gmail.com>
2019-01-30 23:17:04 -0600

    is.even/odd is already vectorized
    Do not need sapply() in examples.

--- R/is.evenodd.R
+++ R/is.evenodd.R
@@ -6,9 +6,9 @@
 #' 
 #' @examples
 #' is.even(1)
-#' sapply(mtcars$gear, is.even)
+#' is.even(mtcars$gear)
 #' is.odd(2)
-#' sapply(mtcars$gear, is.odd)
+#' is.odd(mtcars$gear)
 #' 
 #' @seealso \url{https://github.com/robertschnitman/afp}; `iseven()` and `isodd` in \url{https://docs.julialang.org/en/release-0.4/stdlib/numbers/}
 #' @name is.evenodd
@@ -20,6 +20,3 @@ is.even <- function(x) x %% 2 == 0
 
 #' @rdname is.evenodd
 is.odd <- function(x) x %% 2 == 1
-  
-  
-  
