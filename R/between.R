e11e9602e0bde16a9355036e4da16c821d91cd9f
Robert Schnitman <robertschnitman@gmail.com>
2019-01-30 22:39:49 -0600

    Be closer to SQL's between clause
    WHY: User with a SQL background may expect the function to behave similarly.

--- R/between.R
+++ R/between.R
@@ -1,12 +1,13 @@
-# a la` SQL's between condition.
+### a la` SQL's between condition.
+## (INPUT, OUTPUT) = (vector, boolean)
 
-between <- function(x, low, high, inclusive = FALSE)  {
+between <- function(x, low, high, inclusive = TRUE)  { # SQL's between clause is inclusive (see link below).
     
     if (inclusive == FALSE) {
         
         x > low & x < high
         
-    } else {
+    } else { # https://www.w3schools.com/sql/sql_between.asp
         
         x >= low & x <= high
         
