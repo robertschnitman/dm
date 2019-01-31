c2ed3b9f3e85a02ab8496530174320ffdd1845a9
Robert Schnitman <robertschnitman@gmail.com>
2019-01-30 22:49:40 -0600

    Clarify the expected input/output
    WHY: To minimize debugging on user's side.

--- R/switchv.R
+++ R/switchv.R
@@ -1,6 +1,13 @@
 ### PURPOSE: Vectorize switch.
+## (INPUT, OUTPUT): (vector, vector)
 
 switchv <- function(x, ...) {
+  
+  if (!is.vector(x)) { 
+    
+    stop(paste0('Vector expected. Received ', class(x), '.')) 
+    
+  } # For nth-dimensional data, use switchv within the appropriate functionals.
   
   codes <- function(z) switch(z, ...)
   
