1edce793f603a714a774385f595557076a50d236
Robert Schnitman <robertschnitman@gmail.com>
2019-01-30 23:11:22 -0600

    Type check input
    WHY: Easier debugging.

--- R/reverse.R
+++ R/reverse.R
@@ -1,10 +1,14 @@
-# INPUT/OUTPUT: vector
+### GOAL: Be similar to Julia's map(reverse, x).
+## (INPUT, OUTPUT): (vector, vector)
 
 reverse <- function(x) {
   
   ### GOAL: Split --> (Apply) Reverse --> Combine
   ## The "Split-Apply-Combine" strategy by Hadley Wickham.
   # https://www.jstatsoft.org/article/view/v040i01/v40i01.pdf
+  
+  # 0. Type check.
+  expected(x, 'character')
   
   # 1. Split.
   splits <- mapply(function(s) strsplit(s, split = NULL), x)
