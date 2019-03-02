94fe84f3127c8436423aaacc996ed74041a18281
Robert Schnitman <robertschnitman@gmail.com>
2019-03-02 13:28:23 -0600

    Allow format flexibility
    WHY: Data input may not be numeric. Allow flexibility.

--- R/zero_flag.R
+++ R/zero_flag.R
@@ -1,3 +1,3 @@
 ### PURPOSE: Flag a zero at the beginning of a value if it does not meet a specified width.
 
-zero_flag <- function(x, w) formatC(x, width = w, format = 'd', flag = '0') # e.g. zero_flag(700, 4) == '0700'
+zero_flag <- function(x, w, format = 'd') formatC(x, width = w, format = format, flag = '0') # e.g. zero_flag(700, 4) == '0700'
