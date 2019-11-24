0216faed4da247c3edc888351d2f2f86ee705d7a
Robert Schnitman <robertschnitman@gmail.com>
2019-11-24 12:48:05 -0600

    Add files via upload

--- R/rename_file.R
+++ R/rename_file.R
@@ -1,3 +1,9 @@
+### Renaming files:
+# myfiles   <- list.files()
+# new_names <- gsub('Fall 2018', 'Spring 2019', myfiles)
+# file.rename(myfiles, new_names)
+###
+
 rename_file <- function(filepath,    # Directory of files whose names will be replaced.
                         pattern_now, # String pattern of current filenames.
                         pattern_new  # String to replace the previous pattern.
@@ -12,3 +18,14 @@ rename_file <- function(filepath,    # Directory of files whose names will be re
   file.rename(myfiles, new_names)
   
 }
+### filepath = Desired directory of where the files to be renamed are.
+### pattern_now = regular expression pattern of the file names as they are CURRENTLY.
+### pattern_new = Desired string to replace the file names detected in pattern_now.
+
+# rename_file('J:/IRO/Schnitman/R/RStudio Cheat Sheets','RStudio Cheat Sheet - ', 'RSCS_')
+
+# rename_file('J:/IRO/Snap Webhost Surveys/Schnitman/Nursing/Evaluations/2019/Spring/Data/','Copy', 'Original')
+
+#rename_file('J:/IRO/Snap Webhost Surveys/Schnitman/Nursing/Evaluations/2019/Spring/Data/','2019', '2019v1')
+
+#rename_file('C:/Users/crispychicken/Documents/MEGA/Personal/Money/Taxes/2019/Reports', '_', ' ')
